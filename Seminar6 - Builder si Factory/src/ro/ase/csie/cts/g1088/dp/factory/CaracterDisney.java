f83999a6c533e9f1d83b4676c10bd41f8b727557
CristinaTudor522 <tudorcristina18@stud.ase.ro>
2021-04-01 13:27:40 +0300

    Commit final - Implementare Simple Factory

---
+++ Seminar6 - Builder si Factory/src/ro/ase/csie/cts/g1088/dp/factory/CaracterDisney.java
@@ -0,0 +1,23 @@
+package ro.ase.csie.cts.g1088.dp.factory;
+
+public class CaracterDisney extends CaracterAbstract{
+
+	String culoare;
+	boolean okPtCopiiMici = true;
+	
+	public CaracterDisney(String culoare, String nume) {
+		this.culoare = culoare;
+		this.nume = nume;
+	}
+
+	@Override
+	public void alearga() {
+		System.out.println("Alearga...");
+	}
+
+	@Override
+	public void sare() {
+		System.out.println("Sare...");
+	}
+
+}
